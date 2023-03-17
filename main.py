0db6272be583ca5b41010dccfc098f48786d2139
Ramiro Ramirez-Sanchez <45070898+ramirors1@users.noreply.github.com>
2023-03-17 13:16:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+fruits = ["Apple", "Pear", "Orange"]
+
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+    except IndexError:
+        print("Fruit pie")
+    else:    
+        print(fruit + " pie")
+
+
+make_pie(4)
+
+
